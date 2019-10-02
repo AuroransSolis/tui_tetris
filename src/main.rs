2b31fb227e8830f00fd260a88fe67f8e35e14e2d
Aurorans Solis <primalucegd@gmail.com>
2019-10-02 13:05:27 -0700

    Forgot to change 'termion' to 'crossterm' and also ran the file through rustfmt.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
-extern crate termion;
 extern crate serde;
+extern crate crossterm;
 
 use termion::{
-    screen::AlternateScreen,
-    input::TermRead,
+    color::{self, *},
     event::Key,
-    color::{self, *}
+    input::TermRead,
+    screen::AlternateScreen,
 };
 
-mod tetromino;
-mod gameboard;
 mod game_config;
+mod gameboard;
+mod tetromino;
 
-use tetromino::*;
-use gameboard::*;
 use game_config::*;
+use gameboard::*;
+use tetromino::*;
 
 use std::fs::File;
-use std::io::{stdout, Write, Stdout, stdin, Stdin, Read};
+use std::io::{stdin, stdout, Read, Stdin, Stdout, Write};
 
 fn main() {
     let game_config = if let Ok(mut file) = File::open("tui_tetris.conf") {
@@ -26,13 +26,18 @@ fn main() {
         match toml::from_str(&file_contents) {
             Ok(game_config_precursor) => game_config_precursor,
             Err(e) => {
-                println!("Error loading game config from file! Location: {:?}\nDescription:\n{:?}",
-                    e.line_col(), e);
+                println!(
+                    "Error loading game config from file! Location: {:?}\nDescription:\n{:?}",
+                    e.line_col(),
+                    e
+                );
                 return;
             }
         }
     } else {
-        print!("Could not find game config file! Would you like to use the default config? [y/N]: ");
+        print!(
+            "Could not find game config file! Would you like to use the default config? [y/N]: "
+        );
     };
     let mut screen = AlternateScreen::from(stdout());
     writeln!(screen, "Testing!\n").unwrap();
