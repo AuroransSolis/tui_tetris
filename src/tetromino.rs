0caecf22700460f962d1544bd4aaefd25cc28a2a
Aurorans Solis <primalucegd@gmail.com>
2019-10-06 16:42:42 -0700

    Implemented conversion from u16 to Tetromino.

--- src/tetromino.rs
+++ src/tetromino.rs
@@ -1,7 +1,9 @@
-use super::*;
 use crossterm::Color;
+use rand::{Rng, rngs::ThreadRng};
+use std::hint::unreachable_unchecked;
 
-enum Shape {
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+pub enum Tetromino {
     I,
     J,
     L,
@@ -11,14 +13,17 @@ enum Shape {
     O,
 }
 
-struct Tetromino {
-    c0: (usize, usize),
-    c1: (usize, usize),
-    c2: (usize, usize),
-    c3: (usize, usize),
-    color: Color,
-}
-
-impl Tetromino {
-    pub fn new(shape: Shape, board: &mut GameBoard) {}
+impl From<u16> for Tetromino {
+    fn from(other: u16) -> Self {
+        match other {
+            0 => Tetromino::I,
+            1 => Tetromino::J,
+            2 => Tetromino::L,
+            3 => Tetromino::S,
+            4 => Tetromino::Z,
+            5 => Tetromino::T,
+            6 => Tetromino::O,
+            _ => unsafe { unreachable_unchecked() }
+        }
+    }
 }
